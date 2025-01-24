c219043884d9640cb665a82d5dead837eb738773
vovab <vovab@MASTURBAG>
2025-01-24 18:59:14 +0200

    Добавьте файлы проекта.

---
+++ Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Books
+    {
+        public string Name = "Life";
+        public string Author = "Vladimir";
+        public int Year = 2010;
+        public int Capacity = 240;
+        public void Info()
+        {
+            Console.WriteLine($"Название: {Name}\nАвтор: {Author}\nГод: {Year}\nОбъём: {Capacity}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            books.Info();
+        }
+    }
+}
